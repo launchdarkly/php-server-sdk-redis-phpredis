d73d1257f1f58675b997ec7ef70939f0f8876dc0
Eli Bishop <eli@launchdarkly.com>
2021-07-30 18:15:45 -0700

    add unit test for passing a preconfigured client

--- tests/PHPRedisFeatureRequesterWithClientTest.php
+++ tests/PHPRedisFeatureRequesterWithClientTest.php
@@ -8,8 +8,10 @@ use LaunchDarkly\Integrations\PHPRedis;
 use LaunchDarkly\SharedTest\DatabaseFeatureRequesterTestBase;
 use \Redis;
 
-class PHPRedisFeatureRequesterTest extends DatabaseFeatureRequesterTestBase
+class PHPRedisFeatureRequesterWithClientTest extends DatabaseFeatureRequesterTestBase
 {
+    const CLIENT_PREFIX = 'clientprefix';
+
     /** @var ClientInterface */
     private static $redisClient;
     
@@ -20,8 +22,18 @@ class PHPRedisFeatureRequesterTest extends DatabaseFeatureRequesterTestBase
             'localhost',
             6379,
             null,
-            'RedisFeatureRequesterTest'
+            'RedisFeatureRequesterWithClientTest'
         );
+        self::$redisClient->setOption(\Redis::OPT_PREFIX, self::CLIENT_PREFIX);
+
+        // Setting a prefix parameter on the Redis client causes it to prepend
+        // that string to every key *in addition to* the other prefix that the SDK
+        // integration is applying. This is done transparently so we do not need to
+        // add CLIENT_PREFIX in putItem below. We're doing it so we can be sure
+        // that the PHPRedisFeatureRequester really is using the same client we
+        // passed to it; if it didn't, the tests would fail because putItem was
+        // creating keys with both prefixes but PHPRedisFeatureRequester was
+        // looking for keys with only one prefix.
     }
 
     protected function clearExistingData($prefix): void
@@ -29,6 +41,10 @@ class PHPRedisFeatureRequesterTest extends DatabaseFeatureRequesterTestBase
         $p = self::realPrefix($prefix);
         $keys = self::$redisClient->keys("$p:*");
         foreach ($keys as $key) {
+            if (substr($key, 0, strlen(self::CLIENT_PREFIX)) === self::CLIENT_PREFIX) {
+                // remove extra prefix from the queried keys because del() will re-add it
+                $key = substr($key, strlen(self::CLIENT_PREFIX));
+            }
             self::$redisClient->del($key);
         }
     }
@@ -36,7 +52,8 @@ class PHPRedisFeatureRequesterTest extends DatabaseFeatureRequesterTestBase
     protected function makeRequester($prefix): FeatureRequester
     {
         $factory = PHPRedis::featureRequester([
-            'redis_prefix' => $prefix
+            'redis_prefix' => $prefix,
+            'phpredis_client' => self::$redisClient
         ]);
         return $factory('', '', []);
     }
