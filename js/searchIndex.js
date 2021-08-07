ba4547eb353f9f2c73cac9e5ce7a91079822c28e
LaunchDarklyReleaseBot <launchdarklyreleasebot@launchdarkly.com>
2021-08-07 00:53:34 +0000

    Updating documentation to version 1.0.0

---
+++ js/searchIndex.js
@@ -0,0 +1,39 @@
+Search.appendIndex(
+    [
+                {
+            "fqsen": "\\LaunchDarkly\\Integrations\\PHPRedis",
+            "name": "PHPRedis",
+            "summary": "Integration\u0020with\u0020a\u0020Redis\u0020data\u0020store\u0020using\u0020the\u0020\u0060phpredis\u0060\u0020extension.",
+            "url": "classes/LaunchDarkly-Integrations-PHPRedis.html"
+        },                {
+            "fqsen": "\\LaunchDarkly\\Integrations\\PHPRedis\u003A\u003AfeatureRequester\u0028\u0029",
+            "name": "featureRequester",
+            "summary": "Configures\u0020an\u0020adapter\u0020for\u0020reading\u0020feature\u0020flag\u0020data\u0020from\u0020Redis\u0020using\u0020persistent\u0020connections.",
+            "url": "classes/LaunchDarkly-Integrations-PHPRedis.html#method_featureRequester"
+        },                {
+            "fqsen": "\\",
+            "name": "\\",
+            "summary": "",
+            "url": "namespaces/default.html"
+        },                {
+            "fqsen": "\\LaunchDarkly\\Impl\\Integrations",
+            "name": "Integrations",
+            "summary": "",
+            "url": "namespaces/launchdarkly-impl-integrations.html"
+        },                {
+            "fqsen": "\\LaunchDarkly\\Impl",
+            "name": "Impl",
+            "summary": "",
+            "url": "namespaces/launchdarkly-impl.html"
+        },                {
+            "fqsen": "\\LaunchDarkly",
+            "name": "LaunchDarkly",
+            "summary": "",
+            "url": "namespaces/launchdarkly.html"
+        },                {
+            "fqsen": "\\LaunchDarkly\\Integrations",
+            "name": "Integrations",
+            "summary": "",
+            "url": "namespaces/launchdarkly-integrations.html"
+        }            ]
+);
